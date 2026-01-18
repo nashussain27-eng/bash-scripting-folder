7c0e0428e848eb404ea8b234050432bf01468859
nashussain27-eng <nashussain27@gmail.com>
2026-01-18 20:51:39 +0000

    Update bashlevel3.sh

--- bashlevel3.sh
+++ bashlevel3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ -f "Arena/hero.txt" ]; then
+#-f returns true if the file exists and is a regular file. It is known as a file test operator
     echo "Hero found!"
 else
     echo "Hero missing!"
