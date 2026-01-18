52d0ced65bde4316db62d19038c300bcb38fe5a5
nashussain27-eng <nashussain27@gmail.com>
2026-01-18 22:13:25 +0000

    Update bashlevel4.sh

--- bashlevel4.sh
+++ bashlevel4.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 mkdir -p Backup
+#-p flag tells Bash to create the Backup folder first. It ensure the Backup directory is ready before copying the files into it. 
 cp Arena/*.txt Backup/
+#Copies all the text files from Arena to Backup. *Asterisk is wildcard that acts as a placeholder. It searches for every file thats ends with .txt then matches the "name" part of the files. It then executes by expanding the command to looking like below:
+#cp Arena/archer.txt Arena/hero.txt Arena/mage.txt Arena/warrior.txt Backup/
