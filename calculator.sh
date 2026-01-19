f0fc8cd272093c014ab29ffdd5b5ec644a8541f9
nashussain27-eng <nashussain27@gmail.com>
2026-01-19 20:34:33 +0000

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -16,3 +16,12 @@ echo "Results: $num1 + $num2 = $sum"
 echo "$num1 - $num2 = $sub"
 echo "$num1 - $num2 = $mul"
 echo "$num1 - $num2 = $div"
+
+: '
+read -p Prompts the user. Displays a message and waits for the user to type a value. 
+All four operations $(( .. )) is the standard Bash syntax for integer math
+Division by Zero if [ "$num2 -eq 0" ] prevents the script from crashing by checking the second number before dividing
+echo just basically displays results, prints the final calculation string to the terminal. * is multiplying and / is for dividing
+
+
+'
