89a3f28a7933d9b01d8620a6b08f28e93a04c246
nashussain27-eng <nashussain27@gmail.com>
2026-01-20 21:27:42 +0000

    Update file_ops.sh

--- file_ops.sh
+++ file_ops.sh
@@ -7,3 +7,6 @@ echo "This is a file that was created on the $(date +%Y-%m-%d)" > demo.txt
 
 echo -n "File contents: "
 cat demo.txt
+
+
+#-n flag tells Bash not to create a new line after printing the text. Can use printf to which will print the same output. If I used just echo, the output will be printed on a new line. 
